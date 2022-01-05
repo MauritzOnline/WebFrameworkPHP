9a053e340dcf905cca7f6a7d8831589b278a25e5
Mauritz N <mauritz@europe.com>
2022-01-05 14:21:48 +0200

    Remove comment

--- WebFramework.php
+++ WebFramework.php
@@ -7,7 +7,6 @@
  * @version     0.0.4
  */
 
-// TODO: add cors() method, similar to the helmet() method
 // TODO: add documentation for using WebFrameworkPHP with Nginx
 // TODO: create extensive testing project (will be tested using Postman, or a similar tool)
 
